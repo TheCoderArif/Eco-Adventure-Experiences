7e6b765132a06258b89b4901a70f03fd5d552053
TheCoderArif <mdarifulislam18539@gmail.com>
2026-03-22 13:48:11 +0600

    adding hero section

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import HeroSection from '../pages/HeroSection';
 
 const HomeLayout = () => {
     return (
         <div className='w-11/12 mx-auto'>
             <header>
                 <Navbar></Navbar>
+                <section>
+                    <HeroSection></HeroSection>
+                </section>
             </header>
 
             <main>
---
+++ src/pages/HeroSection.jsx
@@ -0,0 +1,11 @@
+
+const HeroSection = () => {
+    return (
+        <div className="flex flex-col justify-center items-center py-3 "> 
+            <h2 className="text-2xl">Eco-Adventure Experiences</h2>
+            <img className="rounded w-2xl" src="https://i.ibb.co.com/0VQWx8F8/Eco-Adventure-Experiences.png" alt="" />
+        </div>
+    );
+};
+
+export default HeroSection;
