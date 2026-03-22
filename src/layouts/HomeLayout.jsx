07305061e7a02211b50cc0ab7141666a06f003fc
TheCoderArif <mdarifulislam18539@gmail.com>
2026-03-22 13:22:55 +0600

    adding footer

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const HomeLayout = () => {
     return (
@@ -13,7 +14,7 @@ const HomeLayout = () => {
             </main>
 
             <footer>
-
+                <Footer></Footer>
             </footer>
         </div>
     );
